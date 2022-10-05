fefef00ad69273244effcdffd6000cb67209dce7
Invalid <zappeln@gmail.com>
2022-10-04 20:14:10 -0500

    have basic setup for TypeCount, runs successfully

--- src/main/scala/TypeCount.scala
+++ src/main/scala/TypeCount.scala
@@ -4,55 +4,77 @@ import org.apache.hadoop.io.*
 import org.apache.hadoop.util.*
 import org.apache.hadoop.mapred.*
 
-import java.io.IOException
+import java.io.{File, IOException}
 import java.util
 import scala.jdk.CollectionConverters.*
 
 
 
+object MapReduceProgram:
+  class Map extends MapReduceBase with Mapper[LongWritable, Text, Text, IntWritable]:
+    private final val one = new IntWritable(1)
+    private val txt = new Text()
 
-object TypeCount {
+    @throws[IOException]
+    def map(key: LongWritable, value: Text, output: OutputCollector[Text, IntWritable], reporter: Reporter): Unit =
+      val log: String = value.toString
+
+      // Then, for each message type you will produce the number of the generated log messages.
+      for (t <- value.toString().split("\\n")) {
+        if ("INFO".r.findAllIn(t).length > 0) {
+          txt.set("info: ")
+        } else if ("WARN".r.findAllIn(t).length > 0) {
+          txt.set("warn: ")
+        } else if ("DEBUG".r.findAllIn(t).length > 0) {
+          txt.set("debug: ")
+        } else if ("ERROR".r.findAllIn(t).length > 0) {
+          txt.set("error: ")
+        }
+
+        output.collect(txt, one)
+      }
 
 
-  def main(args: Array[String]): Unit = {
-    println("Hello World!")
+  class Reduce extends MapReduceBase with Reducer[Text, IntWritable, Text, IntWritable]:
+    override def reduce(key: Text, values: util.Iterator[IntWritable], output: OutputCollector[Text, IntWritable], reporter: Reporter): Unit =
+      val sum = values.asScala.reduce((valueOne, valueTwo) => new IntWritable(valueOne.get() + valueTwo.get()))
+      output.collect(key,  new IntWritable(sum.get()))
 
+  def main(args: Array[String]): Unit =
+    val conf: JobConf = new JobConf(this.getClass)
+    val input = "log/LogFileGenerator.2022-09-22.log"
+    val output = "reports/report.log"
 
-  }
-}
-
-
-//object MapReduceProgram:
-//  class Map extends MapReduceBase with Mapper[LongWritable, Text, Text, IntWritable]:
-//    private final val one = new IntWritable(1)
-//    private val word = new Text()
-//
-//    @throws[IOException]
-//    def map(key: LongWritable, value: Text, output: OutputCollector[Text, IntWritable], reporter: Reporter): Unit =
-//      val line: String = value.toString
-//      line.split(" ").foreach { token =>
-//        word.set(token)
-//        output.collect(word, one)
-//      }
-//
-//  class Reduce extends MapReduceBase with Reducer[Text, IntWritable, Text, IntWritable]:
-//    override def reduce(key: Text, values: util.Iterator[IntWritable], output: OutputCollector[Text, IntWritable], reporter: Reporter): Unit =
-//      val sum = values.asScala.reduce((valueOne, valueTwo) => new IntWritable(valueOne.get() + valueTwo.get()))
-//      output.collect(key,  new IntWritable(sum.get()))
-//
-//  @main def runMapReduce(inputPath: String, outputPath: String) =
-//    val conf: JobConf = new JobConf(this.getClass)
-//    conf.setJobName("WordCount")
+    conf.setJobName("TypeCount")
 //    conf.set("fs.defaultFS", "local")
-//    conf.set("mapreduce.job.maps", "1")
-//    conf.set("mapreduce.job.reduces", "1")
-//    conf.setOutputKeyClass(classOf[Text])
-//    conf.setOutputValueClass(classOf[IntWritable])
-//    conf.setMapperClass(classOf[Map])
-//    conf.setCombinerClass(classOf[Reduce])
-//    conf.setReducerClass(classOf[Reduce])
-//    conf.setInputFormat(classOf[TextInputFormat])
-//    conf.setOutputFormat(classOf[TextOutputFormat[Text, IntWritable]])
-//    FileInputFormat.setInputPaths(conf, new Path(inputPath))
-//    FileOutputFormat.setOutputPath(conf, new Path(outputPath))
-//    JobClient.runJob(conf)
+    conf.set("mapreduce.job.maps", "5")
+    conf.set("mapreduce.job.reduces", "2")
+
+    conf.setOutputKeyClass(classOf[Text])
+    conf.setOutputValueClass(classOf[IntWritable])
+
+    conf.setMapperClass(classOf[Map])
+
+    conf.setCombinerClass(classOf[Reduce])
+    conf.setReducerClass(classOf[Reduce])
+
+    conf.setInputFormat(classOf[TextInputFormat])
+    conf.setOutputFormat(classOf[TextOutputFormat[Text, IntWritable]])
+
+    FileInputFormat.setInputPaths(conf, new Path(input))
+    FileOutputFormat.setOutputPath(conf, new Path(output))
+    JobClient.runJob(conf)
+
+
+
+
+
+
+//      txt.set("info: ")
+//      output.collect(txt, new IntWritable("INFO".r.findAllIn(log).length))
+//      txt.set("warn: ")
+//      output.collect(txt, new IntWritable("WARN".r.findAllIn(log).length))
+//      txt.set("debug: ")
+//      output.collect(txt, new IntWritable("DEBUG".r.findAllIn(log).length))
+//      txt.set("error: ")
+//      output.collect(txt, new IntWritable("ERROR".r.findAllIn(log).length))
--- src/main/scala/TypeDistribution.scala
+++ src/main/scala/TypeDistribution.scala
@@ -7,6 +7,8 @@ import org.apache.hadoop.mapreduce.*
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat
 
+import java.util.StringTokenizer
+
 
 import java.io.IOException
 import java.util
@@ -20,16 +22,24 @@ object TypeDistribution {
     val word = new Text
 
     def map(key: Any, value: Text, context: Mapper[Object,Text,Text,IntWritable]#Context): Unit = {
+      println("I'M IN MAP_______________________________________________________________" )
       for (t <- value.toString().split("\\s")) {
-        word.set(t)
+        println("PRINTLN: " + t)
+//        word.set(t)
+//        context.write(word, one)
+      }
+
+      val itr = new StringTokenizer(value.toString)
+      while (itr.hasMoreTokens()) {
+        word.set(itr.nextToken())
         context.write(word, one)
       }
     }
   }
 
   class IntSumReducer extends Reducer[Text,IntWritable,Text,IntWritable] {
-    private val result = new IntWritable
-//    override
+    val result = new IntWritable
+
     def reduce(key: Text, values: Iterable[IntWritable], context: Reducer[Text,IntWritable,Text,IntWritable]#Context): Unit = {
       val sum = values.foldLeft(0) { (t, i) => t + i.get }
       context.write(key, new IntWritable(sum))
@@ -38,7 +48,7 @@ object TypeDistribution {
 
   def main(args: Array[String]): Unit = {
     var conf : Configuration = new Configuration
-//    conf.set()
+    
     val input = "log/LogFileGenerator.2022-10-03.log"
     val output = "reports/report.log"
 
@@ -49,15 +59,16 @@ object TypeDistribution {
     job.setCombinerClass(classOf[IntSumReducer])
     job.setReducerClass(classOf[IntSumReducer])
 
+    job.setMapOutputKeyClass(classOf[Text])
+    job.setMapOutputValueClass(classOf[Text])
+
     job.setOutputKeyClass(classOf[Text])
-    job.setOutputValueClass(classOf[IntWritable])
+    job.setOutputValueClass(classOf[Text])
 
     FileInputFormat.addInputPath(job, new Path(input)) // args(0)
     FileOutputFormat.setOutputPath(job, new Path(output)) // args(1)
 
-    if (job.waitForCompletion(true)) 0 else 1
-
-    println("TypeDistribution ran successfully")
+    System.exit(if(job.waitForCompletion(true))  0 else 1)
   }
 }
 
